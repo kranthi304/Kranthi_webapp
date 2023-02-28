2ddc5598004c116849551ddfd7b3d0226ccb9d74
kranthi <98206178+kranthi304@users.noreply.github.com>
2023-02-28 20:35:30 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.header('Kranthi Reddy')
+
+st.title('Btech --- Metallurgical and materials Engineering (Nit Rourkela)')
+
+
+import streamlit as st
+
+st.header(":red[Hii I am Creating My First Streamlit App] ")
+
+st.snow()
+
+btn_click = st.button("Click Here to Know About Vivek Kumar SIngh")
+
+if btn_click == True:
+    st.subheader("I am Vivek Kumar Singh")
+    st.subheader("I am Studying In JCT College Of Engineering And Technology, Combatore,Tamilnadu")
+    st.subheader(":red[Let's Strat Baby]")
+    
